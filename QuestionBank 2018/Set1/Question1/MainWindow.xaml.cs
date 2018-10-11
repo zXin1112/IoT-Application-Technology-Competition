ec1d3066ffed99afa390af29055489142f700614
楪鳴 <yuzuriha.mei@outlook.com>
2018-10-11 16:05:27 +0800

    Q2

--- QuestionBank 2018/Set1/Question1/MainWindow.xaml.cs
+++ QuestionBank 2018/Set1/Question1/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using DigitalLibrary;
+using IPCameraDll;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,9 +23,114 @@ namespace Question1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread thread;
+
+        ADAM4150 adam;
+        IpCameraHelper ipCameraHelper;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            thread = new Thread(MonitorSensor);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //ipCameraHelper1 = new IpCameraHelper("192.168.3.10:81", "admin", "", new Action<ImageEventArgs>(ShowImage));
+            //ipCameraHelper1.StartProcessing();
+
+            adam = new ADAM4150(new ComSettingModel() { DigitalQuantityCom = "COM14" });
+
+            thread.Start();
+
+            if (!Directory.Exists(@"Image\"))
+            {
+                Directory.CreateDirectory(@"Image\");
+            }
+        }
+
+        public void MonitorSensor()
+        {
+            while (true)
+            {
+                adam.SetData();
+
+                if (!adam.DI0)
+                {
+                    if (ipCameraHelper == null)
+                    {
+                        this.Dispatcher.Invoke(new Action(() =>
+                        {
+                            ipCameraHelper = new IpCameraHelper("192.168.3.10:81", "admin", "", new Action<ImageEventArgs>(ShowImage));
+
+                        }));
+                    }
+
+                    ipCameraHelper.StartProcessing();
+                }
+                else
+                {
+                    if (ipCameraHelper != null)
+                        ipCameraHelper.StopProcessing();
+                }
+            }
+        }
+
+        public void ShowImage(ImageEventArgs imageEventArgs)
+        {
+            imgCamera.Source = imageEventArgs.FrameReadyEventArgs.BitmapImage;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var bitmapEncoder = new JpegBitmapEncoder();
+            bitmapEncoder.Frames.Add(BitmapFrame.Create((BitmapImage)imgCamera.Source));
+
+            using (FileStream fileStream = new FileStream(@"Image\" + DateTime.Now.ToString("yyyyMMdd-hhmmss") + ".jpg", FileMode.CreateNew))
+            {
+                bitmapEncoder.Save(fileStream);
+            }
+        }
+
+        private void btnU_MouseDown(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanUp();
+        }
+
+        private void btnU_MouseUp(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanUp();
+        }
+
+        private void btnD_MouseDown(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanDown();
+        }
+
+        private void btnD_MouseUp(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanDown();
+        }
+
+        private void btnL_MouseDown(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanLeft();
+        }
+
+        private void btnL_MouseUp(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanLeft();
+        }
+
+        private void btnR_MouseDown(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanRight();
+        }
+
+        private void btnR_MouseUp(object sender, MouseEventArgs e)
+        {
+            ipCameraHelper.PanRight();
         }
     }
 }
---
+++ QuestionBank 2018/Set1/Question2/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using DigitalLibrary;
+using LEDLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZigBeeLibrary;
+
+namespace Question2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer timer;
+
+        ADAM4150 adam;
+
+        bool OnFire;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            adam = new ADAM4150(new DigitalLibrary.ComSettingModel() { DigitalQuantityCom = "COM14" });
+
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            MonitorADAM();
+            MonitorZigBee();
+        }
+
+        private void MonitorADAM()
+        {
+            ZigBee zigBee = new ZigBee(new ZigBeeLibrary.ComSettingModel() { ZigbeeCom = "COM13" });
+
+            zigBee.GetSet();
+
+            lblTemp.Content = zigBee.lightValue;
+            lblHum.Content = zigBee.temperatureValue;
+            lblLig.Content = zigBee.lightValue;
+
+            double lightNum, limitNum;
+
+            if (double.TryParse(zigBee.lightValue, out lightNum) && double.TryParse(txbLig.Text, out limitNum))
+            {
+                if (lightNum < limitNum)
+                {
+                    adam.OnOff(ADAM4150FuncID.OffDO2);
+                }
+                else
+                {
+                    adam.OnOff(ADAM4150FuncID.OnDO2);
+                }
+            }
+        }
+
+        private void MonitorZigBee()
+        {
+            adam.SetData();
+
+            lblFla.Content = adam.DI1 ? "检测到火焰" : "未检测到火焰";
+            lblSmo.Content = adam.DI2 ? "检测到烟雾" : "未检测到烟雾";
+
+            if (adam.DI1 == OnFire)
+                return;
+
+            OnFire = adam.DI1;
+
+            LEDPlayer ledPlayer = new LEDPlayer("COM11");
+
+            if (adam.DI1)
+            {
+                adam.OnOff(ADAM4150FuncID.OnDO1);
+
+                ledPlayer.DisplayText("厨房着火");
+            }
+            else
+            {
+                adam.OnOff(ADAM4150FuncID.OffDO1);
+
+                ledPlayer.DisplayText("一切正常");
+            }
+        }
+    }
+}
