e68d3d3eb1e090edf57982435c15e0db6b02fef7
楪鳴 <yuzuriha.mei@outlook.com>
2018-10-18 15:51:53 +0800

    Q3

--- QuestionBank 2018/Set4/Question3/MainWindow.xaml.cs
+++ QuestionBank 2018/Set4/Question3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,12 +28,37 @@ namespace Question3
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            cmbPort.ItemsSource = SerialPort.GetPortNames();
         }
 
         private void btnSend_Click(object sender, RoutedEventArgs e)
         {
+            byte[] head = new byte[] { 0xAA, 0x01, 0xBB, 0x51, 0x54 };
+            byte[] type = new byte[] { 1, 2, 100, 0, 100 };
+            byte[] datas = Encoding.Default.GetBytes(txbText.Text);
 
+            byte[] sendData = new byte[datas.Length + 12];
+
+            head.CopyTo(sendData, 0);
+            head[5] = NewMethod(datas);
+            type.CopyTo(sendData, 6);
+            datas.CopyTo(sendData, datas.Length + 6);
+            datas[datas.Length - 1] = 0xFF;
+
+            using (SerialPort serialPort = new SerialPort(cmbPort.SelectedItem.ToString(), 9600))
+            {
+                serialPort.Open();
+                serialPort.Write(datas, 0, datas.Length);
+            }
+        }
+
+        private static byte NewMethod(byte[] datas)
+        {
+            int result = 0;
+            foreach (byte data in datas)
+                result += data;
+
+            return (byte)(result &= 0xFF);
         }
     }
 }
