d85de37492c7c0d734a71392e182a8b122e94819
楪鳴 <yuzuriha.mei@outlook.com>
2018-10-25 10:55:42 +0800

    S5Q1

---
+++ QuestionBank 2018/Set5/Question1/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using IPCameraDll;
+using LEDLibrary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Question1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string ledCom, ledText, cameraIp, recordLength;
+
+        Thread thread;
+
+        IpCameraHelper ipCameraHelper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            thread = new Thread(new ThreadStart(MonitorSensor));
+        }
+
+        public void MonitorSensor()
+        {
+            //adam.SetData();
+
+            if ((int)btnStart.Tag==0)
+            {
+                if (ipCameraHelper == null)
+                {
+                    this.Dispatcher.Invoke(new Action(() =>
+                    {
+                        ipCameraHelper = new IpCameraHelper(cameraIp, "admin", "", new Action<ImageEventArgs>(ShowImage));
+                    }));
+                }
+
+                ipCameraHelper.StartProcessing();
+
+                btnStart.Content = "停止监控";
+                btnStart.Tag = 1;
+            }
+            else
+            {
+                if (ipCameraHelper != null)
+                    ipCameraHelper.StopProcessing();
+
+                btnStart.Content = "开始监控";
+                btnStart.Tag = 0;
+            }
+
+            MonitorSensor();
+        }
+
+        public void ShowImage(ImageEventArgs imageEventArgs)
+        {
+            imgCamera.Source = imageEventArgs.FrameReadyEventArgs.BitmapImage;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("Config.xml"))
+                return;
+
+            XmlDocument document = new XmlDocument();
+            document.Load("Config.xml");
+
+            XmlNode eleLED = document.SelectSingleNode("/root/LED");
+            ledCom = eleLED.Attributes["Com"].Value;
+            ledText = eleLED.Attributes["Text"].Value;
+
+            XmlNode eleCamera = document.SelectSingleNode("/root/Camera");
+            cameraIp = eleCamera.Attributes["IP"].Value;
+            recordLength = eleCamera.Attributes["RecordLength"].Value;
+        }
+
+        private void btnAutoLED_Click(object sender, RoutedEventArgs e)
+        {
+            LEDPlayer player = new LEDPlayer(ledCom);
+            player.DisplayText(ledText);
+        }
+
+        private void btnSetUp_Click(object sender, RoutedEventArgs e)
+        {
+            widSetUp setUp = new widSetUp();
+            setUp.ShowDialog();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            thread.Start();
+        }
+    }
+}
---
+++ QuestionBank 2018/Set5/Question1/widSetUp.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Question1
+{
+    /// <summary>
+    /// widSetUp.xaml 的交互逻辑
+    /// </summary>
+    public partial class widSetUp : Window
+    {
+        public widSetUp()
+        {
+            InitializeComponent();
+
+            cmbLEDCom.ItemsSource = SerialPort.GetPortNames();
+            cmbLEDCom.SelectedIndex = 0;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("Config.xml"))
+                return;
+
+            XmlDocument document = new XmlDocument();
+            document.Load("Config.xml");
+
+            XmlNode eleLED = document.SelectSingleNode("/root/LED");
+            cmbLEDCom.SelectedItem = eleLED.Attributes["Com"].Value;
+            txbLed.Text = eleLED.Attributes["Text"].Value;
+
+            XmlNode eleCamera = document.SelectSingleNode("/root/Camera");
+            txbIP.Text = eleCamera.Attributes["IP"].Value;
+            txbTime.Text = eleCamera.Attributes["RecordLength"].Value;
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            int time;
+
+            if (cmbLEDCom.Items.Count == 0)
+            {
+                MessageBox.Show("无串口选择！");
+                return;
+            }
+
+            if(!int.TryParse(txbTime.Text,out time))
+            {
+                MessageBox.Show("录制时长错误！");
+                return;
+            }
+
+            XmlDocument document = new XmlDocument();
+
+            XmlElement root = document.CreateElement("root");
+            document.AppendChild(root);
+
+            XmlElement eleLED = document.CreateElement("LED");
+            eleLED.SetAttribute("Com", cmbLEDCom.SelectedItem.ToString());
+            eleLED.SetAttribute("Text", txbLed.Text);
+
+            XmlElement eleCamera = document.CreateElement("Camera");
+            eleCamera.SetAttribute("IP", txbIP.Text);
+            eleCamera.SetAttribute("RecordLength", txbTime.Text);
+
+            root.AppendChild(eleLED);
+            root.AppendChild(eleCamera);
+
+            document.Save("Config.xml");
+
+            this.Close();
+        }
+    }
+}
