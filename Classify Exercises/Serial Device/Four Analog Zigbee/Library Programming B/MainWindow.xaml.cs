7e85fc8a48b3c7abd28f3c4a44757ddec1ce7ef3
YuzurihaMei <32050600+YuzurihaMei@users.noreply.github.com>
2018-11-01 11:41:40 +0800

    FourAnalog

--- Classify Exercises/Serial Device/Four Analog Zigbee/Library Programming B/MainWindow.xaml.cs
+++ Classify Exercises/Serial Device/Four Analog Zigbee/Library Programming B/MainWindow.xaml.cs
@@ -34,10 +34,30 @@ namespace Library_Programming_B
         {
             fourAnalogZigbee.GetSet();
 
-            byte[] data = fourAnalogZigbee.OriginalData;
-            byte[] data1 = new byte[] { data[20], data[21] };
-            int value = BitConverter.ToInt16(data1, 0);
-            
+            byte[] datas = fourAnalogZigbee.OriginalData;
+
+            if (datas[1] != 0x16)
+                return;
+
+            byte[] sensorData = new byte[8];
+
+            datas.Skip(18).Take(8).ToArray().CopyTo(sensorData, 0);
+
+            int in1 = BitConverter.ToUInt16(sensorData, 0);
+            int in2 = BitConverter.ToUInt16(sensorData, 2);
+            int in3 = BitConverter.ToUInt16(sensorData, 4);
+
+            lblIN1.Content = in1;
+            lblIN2.Content = in2;
+            lblIN3.Content = in3;
+
+            double light = (((double)in1 * 3300 / 1023 / 150) - 4) / 16 * 20000 + 0;
+            double tem = (((double)in2 * 3300 / 1023 / 150) - 4) / 16 * 70 - 10;
+            double hum = (((double)in3 * 3300 / 1023 / 150) - 4) / 16 * 50 + 50;
+
+            lblLight.Content = light.ToString("f2");
+            lblTem.Content = tem.ToString("f2");
+            lblHum.Content = hum.ToString("f2");
         }
     }
 }
--- Classify Exercises/Serial Device/Four Analog Zigbee/Protocol Programming/MainWindow.xaml.cs
+++ Classify Exercises/Serial Device/Four Analog Zigbee/Protocol Programming/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,60 @@ namespace Protocol_Programming
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort serialPort = null;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            serialPort = new SerialPort("COM1", 38400);
+            serialPort.DataReceived += serialPort_DataReceived;
         }
+
+        void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            while (serialPort.ReadByte() == 0xFE && serialPort.ReadByte() == 0x16)
+            {
+                byte[] datas = new byte[0x1a];
+                datas[0] = 0xfe;
+                datas[1] = 0x16;
+
+                serialPort.Read(datas, 2, 0x1a - 2);
+
+                double in1 = BitConverter.ToUInt16(datas, 18);
+                double in2 = BitConverter.ToUInt16(datas, 20);
+                double in3 = BitConverter.ToUInt16(datas, 22);
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    lblIN1.Content = in1;
+                    lblIN2.Content = in2;
+                    lblIN3.Content = in3;
+
+                    lblLight.Content = ((in1 * 3300 / 1023 / 150 - 4) / 16 * 20000 + 0).ToString("f2");
+                    lblTem.Content = ((in2 * 3300 / 1023 / 150 - 4) / 16 * 70 - 10).ToString("f2");
+                    lblHum.Content = ((in3 * 3300 / 1023 / 150 - 4) / 16 * 50 + 50).ToString("f2");
+                });
+            }
+        }
+
+        private void btnGetData_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)btnGetData.Tag == "0")
+            {
+                serialPort.Open();
+                btnGetData.Content = "停止监听";
+                btnGetData.Tag = "1";
+            }
+            else
+            {
+                serialPort.Close();
+                btnGetData.Content = "开始监听";
+                btnGetData.Tag = "";
+            }
+
+        }
+
+
     }
 }
