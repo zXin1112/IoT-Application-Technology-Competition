08124452cf14699cdf47c58d28f56d3c685a76cd
YuzurihaMei <32050600+YuzurihaMei@users.noreply.github.com>
2018-11-07 09:19:31 +0800

    Cloud Platform MK.I

--- Classify Exercises/Network Device/Cloud Platform/Protocol Programming/MainWindow.xaml.cs
+++ Classify Exercises/Network Device/Cloud Platform/Protocol Programming/MainWindow.xaml.cs
@@ -24,125 +24,132 @@ namespace Protocol_Programming
     /// </summary>
     public partial class MainWindow : Window
     {
-        string token;
+        string token = "";
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        private string RequestApi(string apiAddress, object requestData)
         {
-            Uri uri = new Uri("http://172.18.10.11:81/api/Account/Logon");
+            Uri uri = new Uri("http://172.18.10.11:81/" + apiAddress);
             HttpWebRequest request = HttpWebRequest.CreateHttp(uri);
-            request.Accept = "application/text";
             request.Method = "POST";
-            request.Headers.Add("AccessToken", "");
+            request.Headers.Add("AccessToken", token);
             request.ContentType = "application/json";
 
-            ClientLogonData clientLogonData = new ClientLogonData();
-            clientLogonData.UserName = "user10";
-            clientLogonData.Password = "123456";
-            clientLogonData.ProjectTag = "Test";
-
-            string jsonData = JsonConvert.SerializeObject(clientLogonData);
-            txbJson.Text = jsonData;
+            string jsonData = JsonConvert.SerializeObject(requestData);
             byte[] data = Encoding.UTF8.GetBytes(jsonData);
 
             using (Stream stream = request.GetRequestStream())
                 stream.Write(data, 0, data.Length);
 
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            StreamReader streamReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-            string showdata = streamReader.ReadToEnd();
-            txbJson.Text = showdata;
-            streamReader.Close();
 
-            OperationResult<ClientLogonResult> result = JsonConvert.DeserializeObject<OperationResult<ClientLogonResult>>(showdata);
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                return streamReader.ReadToEnd();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string jsonData = RequestApi("api/Account/Logon", new ClientLogonData() { UserName = "user10", Password = "123456", ProjectTag = "Test" });
+
+            OperationResult<ClientLogonResult> result = JsonConvert.DeserializeObject<OperationResult<ClientLogonResult>>(jsonData);
+
+            if (result.Status != ResultStatus.Success)
+                return;
+
             ClientLogonResult clientLogonResult = result.AppendData;
-            txbJson.Text = clientLogonResult.AccessToken;
             token = clientLogonResult.AccessToken;
         }
 
+
+
         private void btnSensorData_Click(object sender, RoutedEventArgs e)
         {
-            Uri uri = new Uri("http://172.18.10.11:81/api/SensorData/GetNewestSensorData?sensorId=135");
-            HttpWebRequest request = HttpWebRequest.CreateHttp(uri);
-            request.Accept = "application/text";
-            request.Method = "POST";
-            request.Headers.Add("AccessToken", token);
-            request.ContentType = "application/json";
+            string jsonData = RequestApi("api/SensorData/GetNewestSensorDatas", new int[] { 135, 137, 139, 140, 138, 136 });
 
-            string jsonData = JsonConvert.SerializeObject("123");
-            //txbTest.Text = jsonData;
-            byte[] data = Encoding.UTF8.GetBytes(jsonData);
+            OperationResult<List<SensorDataModel>> result = JsonConvert.DeserializeObject<OperationResult<List<SensorDataModel>>>(jsonData);
+            List<SensorDataModel> sensorDataModel = result.AppendData;
 
-            using (Stream stream = request.GetRequestStream())
-                stream.Write(data, 0, data.Length);
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            StreamReader streamReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-            string showdata = streamReader.ReadToEnd();
-            txbJson.Text = showdata;
-            streamReader.Close();
-
-            OperationResult<SensorDataModel> result = JsonConvert.DeserializeObject<OperationResult<SensorDataModel>>(showdata);
-            SensorDataModel sensorDataModel = result.AppendData;
-            txbJson.Text = sensorDataModel.SensorData + sensorDataModel.SensorUnit;
-
-            //Uri uri2 = new Uri("http://172.18.10.11:81/api/Device/GetSensorList");
-            //HttpWebRequest request2 = HttpWebRequest.CreateHttp(uri2);
-            //request2.Accept = "application/text";
-            //request2.Method = "POST";
-            //request2.Headers.Add("AccessToken", token);
-            ////request.Headers.Add("AccessToken", "");
-            //request2.ContentType = "application/json";
-
-            //string jsonData2 = JsonConvert.SerializeObject("AAA");
-            ////txbTest.Text = jsonData;
-            //byte[] data2 = Encoding.UTF8.GetBytes(jsonData2);
-
-            //Stream stream2 = request2.GetRequestStream();
-            ////stream.Write(data, 0, data.Length);
-            //stream.Close();
-
-            //HttpWebResponse response2 = (HttpWebResponse)request2.GetResponse();
-            //StreamReader streamReader = new StreamReader(response2.GetResponseStream(), Encoding.UTF8);
-            //string showdata = streamReader.ReadToEnd();
-            //txbTest.Text = showdata;
-            //streamReader.Close();
-
-            //OperationResult result = JsonConvert.DeserializeObject<OperationResult>(showdata);
-            //List<SensorModelBase> lsSensorModelBase = JsonConvert.DeserializeObject<List<SensorModelBase>>(result.AppendData.ToString());
-            ////txbTest.Text = sensorDataModel.SensorData + sensorDataModel.SensorUnit;
+            foreach (SensorDataModel model in sensorDataModel)
+            {
+                switch (model.SensorId)
+                {
+                    case 135: lbl02IN1.Content = model.SensorData + " " + model.SensorUnit; break;
+                    case 137: lbl02IN2.Content = model.SensorData + " " + model.SensorUnit; break;
+                    case 139: lbl01IN2.Content = model.SensorData + " " + model.SensorUnit; break;
+                    case 140: lbl01IN3.Content = model.SensorData + " " + model.SensorUnit; break;
+                    case 138: lbl02IN3.Content = model.SensorData + " " + model.SensorUnit; break;
+                    case 136: lbl01IN4.Content = model.SensorData + " " + model.SensorUnit; break;
+                }
+            }
         }
 
-        private void btnFan01_Click(object sender, RoutedEventArgs e)
+        private void btnLamp01_Click(object sender, RoutedEventArgs e)
         {
-            Uri uri = new Uri("http://172.18.10.11:81/api/Device/OperationActuator?actuatorId=1&actuatorStatus=0");
-            HttpWebRequest request = HttpWebRequest.CreateHttp(uri);
-            request.Accept = "application/text";
-            request.Method = "POST";
-            request.Headers.Add("AccessToken", token);
-            request.ContentType = "application/json";
+            if (btnLamp01.Tag.ToString() == "0")
+            {
+                string jsonData = RequestApi("api/Device/OperationActuator?actuatorId=1&actuatorStatus=1", " ");
 
-            string jsonData = JsonConvert.SerializeObject("123");
-            //txbTest.Text = jsonData;
-            byte[] data = Encoding.UTF8.GetBytes(jsonData);
+                OperationResult<string> result = JsonConvert.DeserializeObject<OperationResult<string>>(jsonData);
 
-            Stream stream = request.GetRequestStream();
-            stream.Write(data, 0, data.Length);
-            stream.Close();
+                if (result.Status == ResultStatus.Success)
+                {
+                    btnLamp01.Tag = "1";
+                    btnLamp01.Content = "关";
+                }
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            StreamReader streamReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-            string showdata = streamReader.ReadToEnd();
-            txbJson.Text = showdata;
-            streamReader.Close();
+                txbJson.Text = result.Message;
+            }
+            else
+            {
+                string jsonData = RequestApi("api/Device/OperationActuator?actuatorId=1&actuatorStatus=0", " ");
 
-            OperationResult<string> result = JsonConvert.DeserializeObject<OperationResult<string>>(showdata);
-            string sensorDataModel = result.AppendData;
-            txbJson.Text = sensorDataModel;
+                OperationResult<string> result = JsonConvert.DeserializeObject<OperationResult<string>>(jsonData);
+
+                if (result.Status == ResultStatus.Success)
+                {
+                    btnLamp01.Tag = "0";
+                    btnLamp01.Content = "开";
+                }
+
+                txbJson.Text = result.Message;
+
+            }
+        }
+
+        private void btnLamp02_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnLamp02.Tag.ToString() == "0")
+            {
+                string jsonData = RequestApi("api/Device/OperationActuator?actuatorId=2&actuatorStatus=1", " ");
+
+                OperationResult<string> result = JsonConvert.DeserializeObject<OperationResult<string>>(jsonData);
+
+                if (result.Status == ResultStatus.Success)
+                {
+                    btnLamp02.Tag = "1";
+                    btnLamp02.Content = "关";
+                }
+
+                txbJson.Text = result.Message;
+            }
+            else
+            {
+                string jsonData = RequestApi("api/Device/OperationActuator?actuatorId=2&actuatorStatus=0", " ");
+
+                OperationResult<string> result = JsonConvert.DeserializeObject<OperationResult<string>>(jsonData);
+
+                if (result.Status == ResultStatus.Success)
+                {
+                    btnLamp02.Tag = "0";
+                    btnLamp02.Content = "开";
+                }
+
+                txbJson.Text = result.Message;
+            }
         }
     }
 }
